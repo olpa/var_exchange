a3caef7e308dc40cae6b9f73f332d0961684a298
Oleg Parashchenko <olpa@uucode.com>
2022-06-28 11:05:38 +0200

    Use a command line.

--- kd_run.py
+++ kd_run.py
@@ -1,13 +1,24 @@
+import argparse
 import dropbox
 import os
 import sys
 
 import kaggle_dropbox as kd
 
-auth = kd.get_auth()
+def parse_command_line():
+    parser = argparse.ArgumentParser(description = 'Exchange Python variables through Dropbox')
+    parser.add_argument('--basedir', type=str, help='parent directory on Dropbox')
+    parser.add_argument('--get-file', type=str)
+    parser.add_argument('--put-file', type=str)
+    parser.add_argument('--get-var', type=str, help='get file from dropbox and print as python variable')
+    args = parser.parse_args()
+    assert bool(args.get_file) + bool(args.put_file) + bool(args.get_var) == 1, "One and only one of the arguments '--get-file', '--put-file' and '--get-var' is expected, got:" + str(args)
+    return args
+
+#auth = kd.get_auth()
 # print(auth)
 # kd.save_auth(auth)
-token = kd.auth_to_token(auth)
+#token = kd.auth_to_token(auth)
 # print(dir(token))
 
 # refresh token:
@@ -15,10 +26,10 @@ token = kd.auth_to_token(auth)
 # https://github.com/dropbox/dropbox-sdk-python/blob/main/example/oauth/commandline-oauth-scopes.py#L79
 
 
-DROPBOX_ACCESS_TOKEN = token.access_token
+#DROPBOX_ACCESS_TOKEN = token.access_token
 
-dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
-print(dbx)
+#dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
+#print(dbx)
 # print(dir(dbx))
 #print(dbx.files_list_folder('/olpa-kaggle'))
 #print(dbx.files_list_folder('/'))
@@ -45,4 +56,10 @@ def download_file(fname):
 
 
 # upload_file('test.py')
-download_file('test.py')
+# download_file('test.py')
+
+def main():
+    args = parse_command_line()
+    print(args)
+
+main()
