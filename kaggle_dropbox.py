b96d1f1c8f793c69fd9ae768a80614d24ba95eda
Oleg Parashchenko <olpa@uucode.com>
2022-07-04 05:15:51 +0200

    Lint.

--- kaggle_dropbox.py
+++ kaggle_dropbox.py
@@ -21,6 +21,7 @@ def get_auth_from_file(fname):
         assert required in auth, f"Auth json should have '{required}' key"
     return auth
 
+
 def get_auth_from_secret(sname):
     try:
         import kaggle_secrets
@@ -38,12 +39,6 @@ def get_auth_from_secret(sname):
     for required in ('key', 'secret', 'oauth'):
         assert required in auth, f"Auth json should have '{required}' key"
     return auth
-
-def save_auth(auth):
-    fname = os.path.expanduser(fname)
-    with open(fname, 'w') as h:
-        json.dump(auth, h, indent=2)
-        h.write('\n')
 
 
 def auth_first_time():
@@ -68,23 +63,9 @@ def auth_first_time():
         'oauth': s_oauth,
         'secret': secret,
         })
-    print(f"Put the following json to the file '{DEFAULT_AUTH_FILE}' or kaggle kernel secret '{DEFAULT_SECRET_NAME}':\n{s_json}")
+    print(f"Put the following json to the file '{DEFAULT_AUTH_FILE}' or " +
+          f"kaggle kernel secret '{DEFAULT_SECRET_NAME}':\n{s_json}")
 
-def refresh_token(auth):
-    prev_result = auth_to_token(auth)
-    dbx = dropbox.Dropbox(
-            oauth2_access_token=prev_result.access_token,
-            oauth2_access_token_expiration=prev_result.expires_at,
-            oauth2_refresh_token=prev_result.refresh_token,
-            app_key=auth['key'],
-            app_secret=auth['secret']
-            )
-    return dbx
-
-# ar = auth_first_time(...)
-# import pickle
-# auth['oauth'] = base64.b64encode(b).decode('ascii')
-# kd.save_auth(auth)
 
 def join_path(basedir, fname):
     if fname and fname[0] == '/':
@@ -93,24 +74,29 @@ def join_path(basedir, fname):
     slash = '/' if basedir and basedir[0] != '/' else ''
     return f'{slash}{basedir}/{fname}'
 
+
 def extract_oauth_object(auth):
     oauth = auth['oauth']
     pickled = base64.b64decode(oauth)
     return pickle.loads(pickled)
 
+
 class KaggleDropbox:
     def __init__(self,
-            basedir='',
-            auth_file=DEFAULT_AUTH_FILE,
-            auth_secret=DEFAULT_SECRET_NAME,
-            ):
+                 basedir='',
+                 auth_file=DEFAULT_AUTH_FILE,
+                 auth_secret=DEFAULT_SECRET_NAME,
+                 ):
         self.basedir = basedir
         auth = get_auth_from_file(auth_file)
         if not auth:
             auth = get_auth_from_secret(auth_secret)
-        assert auth, f"Auth not found. Looked for the file {auth_file} and kaggle secret '{auth_secret}'"
+        assert auth, ("Auth not found. Looked for the file " +
+                      f"{auth_file} and kaggle secret '{auth_secret}'")
         oauth = extract_oauth_object(auth)
-        assert isinstance(oauth, dropbox.oauth.OAuth2FlowNoRedirectResult), "key 'oauth' in auth, expected type: dropbox.oauth.OAuth2FlowNoRedirectResult, got: {type(oauth)}"
+        assert isinstance(oauth, dropbox.oauth.OAuth2FlowNoRedirectResult), \
+               ("key 'oauth' in auth, expected type: " +
+               "dropbox.oauth.OAuth2FlowNoRedirectResult, got: {type(oauth)}")
         self.dbx = dropbox.Dropbox(
                 oauth2_access_token=oauth.access_token,
                 oauth2_access_token_expiration=oauth.expires_at,
@@ -122,7 +108,7 @@ class KaggleDropbox:
     def get_file_content(self, fname):
         path = join_path(self.basedir, fname)
         try:
-            metadata, res  = self.dbx.files_download(path)
+            metadata, res = self.dbx.files_download(path)
         except dropbox.exceptions.ApiError as e:
             logger.info("Can not get '%s': '%s'", path, e)
             e = e.error
@@ -135,7 +121,8 @@ class KaggleDropbox:
 
     def put_file(self, content, fname):
         path = join_path(self.basedir, fname)
-        self.dbx.files_upload(content, path,
+        self.dbx.files_upload(
+                content, path,
                 mode=dropbox.files.WriteMode.overwrite)
 
     def putv(self, var, fname):
@@ -147,10 +134,13 @@ class KaggleDropbox:
         if content:
             return pickle.loads(content)
 
+
 def get_putv_getv(**kw):
     kd = KaggleDropbox(**kw)
+
     def putv(*ls):
         kd.putv(*ls)
+
     def getv(*ls):
         return kd.getv(*ls)
     return putv, getv
--- kd_run.py
+++ kd_run.py
@@ -1,64 +1,31 @@
 import argparse
-import dropbox
 import logging
-import os
 import sys
-
 from kaggle_dropbox import KaggleDropbox, auth_first_time
 
+
 def parse_command_line():
-    parser = argparse.ArgumentParser(description = 'Exchange Python variables through Dropbox')
-    parser.add_argument('--setup', dest='do_setup', action='store_true', help='create auth file')
-    parser.add_argument('--basedir', type=str, help='parent directory on Dropbox')
+    parser = argparse.ArgumentParser(
+            description='Exchange Python variables through Dropbox')
+    parser.add_argument(
+            '--setup', dest='do_setup',
+            action='store_true', help='create auth file')
+    parser.add_argument(
+            '--basedir', type=str,
+            help='parent directory on Dropbox')
     parser.add_argument('--get-file', type=str)
     parser.add_argument('--put-file', type=str)
-    parser.add_argument('--get-var', type=str, help='get file from dropbox and print as python variable')
+    parser.add_argument(
+            '--get-var', type=str,
+            help='get file from dropbox and print as python variable')
     args = parser.parse_args()
-    assert bool(args.do_setup) + bool(args.get_file) + bool(args.put_file) + bool(args.get_var) == 1, "One and only one of the arguments '--setup', '--get-file', '--put-file' and '--get-var' is expected, got:" + str(args)
+    n_excl = bool(args.do_setup) + bool(args.get_file) + \
+        bool(args.put_file) + bool(args.get_var)
+    assert n_excl == 1, "One and only one of the arguments '--setup', " + \
+           "'--get-file', '--put-file' and '--get-var' is expected, got:" + \
+           str(args)
     return args
 
-#auth = kd.get_auth()
-# print(auth)
-# kd.save_auth(auth)
-#token = kd.auth_to_token(auth)
-# print(dir(token))
-
-# refresh token:
-# https://github.com/dropbox/dropbox-sdk-python/blob/main/example/oauth/commandline-oauth-scopes.py
-# https://github.com/dropbox/dropbox-sdk-python/blob/main/example/oauth/commandline-oauth-scopes.py#L79
-
-
-#DROPBOX_ACCESS_TOKEN = token.access_token
-
-#dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
-#print(dbx)
-# print(dir(dbx))
-#print(dbx.files_list_folder('/olpa-kaggle'))
-#print(dbx.files_list_folder('/'))
-#print(dbx.files_list_folder('olpa-kaggle'))
-#print(dbx.files_list_folder('kaggle'))
-#print(dbx.files_list_folder('', recursive=True))
-
-def make_dbx_name(fname):
-    return f'/olpa-kaggle/{os.path.basename(fname)}-py'
-
-def upload_file(fname):
-    with open(fname, 'rb') as h:
-        dbx.files_upload(h.read(), make_dbx_name(fname))
-
-def download_file(fname):
-    dbx_name = make_dbx_name(fname)
-    print('Download:', dbx_name, '->', fname + '.dbx')
-    metadata, res  = dbx.files_download(dbx_name)
-    print('meta:', metadata)
-    print('res:', res)
-    print('res.content:', res.content)
-    with open(fname + '.dbx', 'wb') as h:
-        h.write(res.content)
-
-
-# upload_file('test.py')
-# download_file('test.py')
 
 def main():
     args = parse_command_line()
@@ -83,5 +50,6 @@ def main():
         print(v)
         sys.exit(0)
     assert False
+
 
 main()
